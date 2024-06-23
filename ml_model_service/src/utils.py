e60f116181f3f74a611314231b933b90a291b53d
tekhin <tekhin@cvisionlab.com>
2024-06-23 12:04:51 +0300

    feat: add new generation logic

--- ml_model_service/src/utils.py
+++ ml_model_service/src/utils.py
@@ -1,63 +1,81 @@
 import random
 
 
-genders = ['well dressed male', 'well dressed female', "image"]
 art_styles = ['Photorealism', 'Classicism', 'Land Art', 'Realism', 'modernist']
 resolutions = ['unreal engine', 'sharp focus', '8k', 'vray']
 lighting = ['cinematic', 'dark', 'sunlight', 'god rays']
 required_colors = ['light salmon', 'pale turquoise', 'royal blue']
 locations = ['urban', 'countryside', 'beach', 'mountain', 'forest']
 landscapes = ['cityscape', 'seascape', 'mountainscape', 'countryscape', 'sunset']
-activities = ['sport', 'work', 'relaxation', 'travel', 'entertainment']
+activities = ['sport', 'work', 'relaxation', 'travel', 'entertainment', 'meditation', 'exploration', 'buisness']
 attributes = ['mobile phone', 'coffee', 'book',  'car', 'house']
 
 
-def generate_prompt(age: int = 30) -> str:
+def generate_prompt(age: int = 30, sex: str = '', product: str = 'people', custom_prompt: str = '') -> str:
     """
     Generate a prompt for the ML model.
     
     Parameters
     ----------
     age : int
-        The age of the person.
+        The age of the subject.
+    sex : str
+        The sex of the subject ('male' or 'female' or '').
+    product : str
+        The product category for generation ('people', 'car', 'house', 'credit card').
+    custom_prompt : str
+        Custom words to include in the generation prompt.
     
     Returns
     -------
     str
         The generated prompt.
     """
-    if age < 11:
-        age_group = '0-10'
-    elif age < 18:
-        age_group = '10-17'
-    elif age < 26:
-        age_group = '17-25'
-    elif age < 51:
-        age_group = '25-45'
+    age_group = ''
+    if 'people' in product and sex:
+        gender = f"a well dressed {sex.lower()}"
+        if age < 11:
+            age_group = 'child'
+        elif age < 18:
+            age_group = 'teenager'
+        elif age < 26:
+            age_group = 'young adult'
+        elif age < 51:
+            age_group = 'adult'
+        else:
+            age_group = 'senior'
     else:
-        age_group = '45-65'
-    
-    gender = random.choice(genders)
-    art_style = random.choice(art_styles)
-    resolution = random.choice(resolutions)
-    lighting_style = random.choice(lighting)
-    location = random.choice(locations)
-    landscape = random.choice(landscapes)
-    activity = random.choice(activities)
-    attribute = random.choice(attributes)
+        gender = "an object"
+        age_group = ""
 
-    prompt = f"""The image must include the colors {', '.join(required_colors)} :Depict a {gender} in their {age_group} engaging in {activity} activities, 
-    such as {activity} in a {location} {landscape} with a {attribute}, using a {art_style} visual style. 
-    The artwork should be rendered in {resolution} and {lighting_style} lighting. 
-    """
+    if 'car' in product:
+        attribute = "a vehicle"
+        activity = "parked"
+    elif 'house' in product:
+        attribute = "a building"
+        activity = "inhabited"
+    elif 'credit card' in product:
+        attribute = "a payment card without digits"
+        activity = "being used"
+    else:
+        attribute = random.choice(attributes)
+        activity = random.choice(activities)
+
+    prompt = f"The image must include the colors {', '.join(required_colors)}.{custom_prompt} Depict {gender} {age_group} involved in {activity} with {attribute}."
+    prompt += f" The scene should be in a {random.choice(locations)} setting, rendered in {random.choice(resolutions)} resolution."
+    prompt += f" Ensure the use of {random.choice(lighting)} lighting and {random.choice(art_styles)} style."
 
     return prompt
 
 
-
-def generate_negative_prompt() -> str:
+def generate_negative_prompt(product: str = 'people') -> str:
     """
     Generate a negative prompt for the ML model.
+    
+    Parameters
+    ----------
+    product : str
+        The category for generation.
     
     Returns
     -------
@@ -74,4 +92,9 @@ def generate_negative_prompt() -> str:
     The hands and neck are not malformed, and there are no extra heads or out-of-frame elements. 
     The artwork is not low-res or disgusting and is a well-drawn, highly detailed, and beautiful rendering.
     """
+    if 'people' in product:
+        negative_prompt = "avoid any depiction of animals objects." + negative_prompt
+    elif 'product' in product:
+        negative_prompt = "avoid any depiction of humans or human-like figures." + negative_prompt
+        
     return negative_prompt
