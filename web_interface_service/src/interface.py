272dbd6fac196bad4fb200b9559781f91c7fc7c4
tekhin <tekhin@cvisionlab.com>
2024-06-23 15:03:10 +0300

    feat: add intsructions

--- web_interface_service/src/interface.py
+++ web_interface_service/src/interface.py
@@ -5,6 +5,7 @@ from io import BytesIO
 import base64
 import pandas as pd
 import psycopg2
+import logging
 import time
 from . import config
 
@@ -19,10 +20,10 @@ def wait_for_db():
         try:
             conn = psycopg2.connect(**config.DB_CONFIG)
             conn.close()
-            print("Database is available.")
+            logging.info("Database is available.")
             break
         except Exception as e:
-            print(f"Database not available yet, waiting... Error: {e}")
+            logging.info(f"Database not available yet, waiting... Error: {e}")
             time.sleep(5)
 
 def generate_image(user_id: str, metadata: dict) -> Image:
@@ -87,63 +88,14 @@ with gr.Blocks() as demo:
     input_data = gr.Textbox(label="User ID")
     metadata_input = gr.Textbox(label="Metadata", value="{'age': 20, 'sex': 'male', 'product': 'people', 'custom_prompt': '', 'height': 720, 'width': 720}")
     output_data = gr.Image()
+    mark_result_button = gr.Button("Send Mark image to database")
     history_viewer = gr.Dataframe(headers=["id", "image_url", "metadata", "user_id"], datatype=["str", "str", "str", "str"])
     get_history_button = gr.Button("Get History by user ID")
-    mark_result_button = gr.Button("Send Mark image to database")
-
+    
     get_history_button.click(get_history, inputs=input_data, outputs=history_viewer)
     mark_result_button.click(mark_image, inputs=[input_data, gr.Checkbox(label="Mark As Good Image")], outputs=None)
-    instruction_md = gr.Markdown(
-        """
-        # Инструкция по использованию сервиса:
 
-        ## Метаданные
-        Метаданные должны содержать следующие поля:
 
-        - `user_id`: Идентификатор пользователя. Может быть и числом и строкой.
-        - `sex`: Пол пользователя. Может быть либо 'male' либо 'female' можно оставить пустым.
-        - `age`: Возраст пользователя. число.
-        - `product`: Категория продукта. Может быть либо 'people', либо 'car', либо 'house', либо 'credit card' можно оставить пустым.
-        - `custom_prompt`: Пользовательский промпт. Может быть либо строкой либо пустым. Добавит на генерацию предметы или свойства предметов.
-        - `height`: Высота изображения. Число.
-        - `width`: Ширина изображения. Число.
-        
-        ## Пример метаданных:
-        ```
-        {
-            "user_id": "123456",
-            "sex": "male",
-            "age": 20,
-            "product": "car",
-            "custom_prompt": "engaging in driving",
-            "height": 720,
-            "width": 720
-        }
-        ```
-        
-        ```
-        {
-            "user_id": "123456",
-            "product": "house",
-            "custom_prompt": "beautiful country house",
-            "height": 480,
-            "width": 1156
-        }
-        ```
-
-        ## Генерация изображения
-        
-        Для генерации изображения необходимо ввести метаданные в поле `metadata` и имя пользователя в поле `user_id` и нажать на кнопку `Generate Image`.
-        
-        ## Разметка изображения
-        
-        Для разметки изображения необходимо ввести метаданные в поле `metadata` и нажать на кнопку `Generate Image`. После если изображение хоршее и понравилось нажать на кнопку `Mark As Good Image`(выставляется метка True) или оставить чекбокс пустым(False). Отправить изображение в базу данных, нажать на кнопку `Send Mark image to database`.
-        
-        ## Получение истории сгенерированных изображений для пользователя
-        
-        Ввести в поле `user_id` имя пользователя для которого нужно получить историю и нажать на кнопку `Get History by user ID`. В вверху странице появится таблица с историей сгенерированных изображений для данного пользователя.
-        """
-    )
 
     interface = gr.Interface(
         fn=generate_image,
@@ -151,6 +103,67 @@ with gr.Blocks() as demo:
         outputs=[output_data],
         live=False
     )
+    gr.Markdown(
+            """
+            # Инструкция по использованию сервиса:
+            
+            ## Идентификатор пользователя
+                - `user_id`: Идентификатор пользователя. Например `Test user`. Может быть любым.
+
+
+            ## Метаданные
+            Метаданные могут содержать следующие поля:
+
+            - `sex`: Пол пользователя. Может быть либо 'male' либо 'female' можно оставить пустым.
+            - `age`: Возраст пользователя. число.
+            - `product`: Категория продукта. Может быть либо 'people', либо 'car', либо 'house', либо 'credit card' можно оставить пустым.
+            - `custom_prompt`: Пользовательский промпт. Может быть либо строкой либо пустым. Добавит на генерацию предметы или свойства предметов.
+            - `height`: Высота изображения. Число. (сумма высоты и ширины должна быть не больше 3500, из-за использования слабой видеокарты.)
+            - `width`: Ширина изображения. Число. (сумма высоты и ширины должна быть не больше 3500, из-за использования слабой видеокарты.)
+            
+            ## Пример метаданных:
+            1) 
+            ```
+            {
+                "sex": "male",
+                "age": 20,
+                "product": "car",
+                "custom_prompt": "engaging in driving",
+                "height": 720,
+                "width": 720
+            }
+            ```
+            2) 
+            ```
+            {
+                "sex": "male",
+                "age": 20,
+                "height": 480,
+                "width": 1160
+            }
+            ```
+            3) 
+            ```
+            {
+                "product": "house",
+                "custom_prompt": "beautiful country house",
+                "height": 1080,
+                "width": 1080
+            }
+            ```
+
+            ## Генерация изображения
+            
+            Для генерации изображения необходимо ввести метаданные в поле `metadata` и имя пользователя в поле `user_id` и нажать на кнопку `Generate Image`.
+            
+            ## Разметка изображения
+            
+            Для разметки изображения необходимо ввести метаданные в поле `metadata` и нажать на кнопку `Generate Image`. После если изображение хоршее и понравилось нажать на кнопку `Mark As Good Image`(выставляется метка True) или оставить чекбокс пустым(False). Отправить изображение в базу данных, нажать на кнопку `Send Mark image to database`.
+            
+            ## Получение истории сгенерированных изображений для пользователя
+            
+            Ввести в поле `user_id` имя пользователя для которого нужно получить историю и нажать на кнопку `Get History by user ID`. В вверху странице появится таблица с историей сгенерированных изображений для данного пользователя.
+            """)
 
 if __name__ == '__main__':
     demo.launch(server_name="0.0.0.0", server_port=config.SERVICE_PORT)
